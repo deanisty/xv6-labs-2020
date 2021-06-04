fbfb5e89c48542f0e5af71e6be6e38c9da419c56
ubuntu <ubuntu@deepdoo.com>
2021-06-04 23:53:10 +0800

    add find

---
+++ user/find.c
@@ -0,0 +1,9 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[]) {/*{{{*/
+    printf("%s\n", "find find...");
+    exit(1);
+}/*}}}*/
